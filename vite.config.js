82f8340e0a5b861153b4100ae1dbedd58637ea99
savinglegaciespr-coder <savinglegaciespr@gmail.com>
2026-04-26 22:57:48 -0400

    Enhance Vite config with esbuild options

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-export default defineConfig({ plugins: [react()] })
+
+export default defineConfig({
+  plugins: [react()],
+  esbuild: {
+    include: /src\/.*\.[jt]sx?$/,
+    exclude: [],
+    loader: 'jsx'
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx'
+      }
+    }
+  }
+})
